d8d97d6fa1dcd55e2cb419aef9ca090339421b69
sarah <sarah-holderness@pluralsight.com>
2017-06-01 13:32:37 -0400

    Launching the DetailActivity with an Intent

--- CandyCoded/app/src/main/java/com/codeschool/candycoded/MainActivity.java
+++ CandyCoded/app/src/main/java/com/codeschool/candycoded/MainActivity.java
@@ -1,6 +1,7 @@
 package com.codeschool.candycoded;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -60,7 +61,8 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                
+                Intent detailIntent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(detailIntent);
             }
         });
     }
