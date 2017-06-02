7de77db03669835f050b7a879ce38922d75be045
sarah <sarah-holderness@pluralsight.com>
2017-06-02 10:52:37 -0400

    Created a new Candy class.  Added GSON as a dependency so that we could use it to parse the JSON response into an Array of Candy objects.

---
+++ CandyCoded/app/src/main/java/com/codeschool/candycoded/Candy.java
@@ -0,0 +1,9 @@
+package com.codeschool.candycoded;
+
+public class Candy {
+    public int id;
+    public String name;
+    public String imageURL;
+    public String price;
+    public String description;
+}
